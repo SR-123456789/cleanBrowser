99ef5d8636f759a1276eedd864fb0fd640df1377
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2025-08-01 20:10:22 +0900

    Initial Commit

---
+++ cleanBrowser/cleanBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  cleanBrowserApp.swift
+//  cleanBrowser
+//
+//  Created by NakataniSoshi on 2025/08/01.
+//
+
+import SwiftUI
+
+@main
+struct cleanBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
